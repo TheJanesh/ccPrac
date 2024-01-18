e502ae71c8a6e0d56b4f875fa6c2c44202be41bb
Janesh <kuckian.janesh18824@gmail.com>
2024-01-18 09:57:20 +0530

    after modification commit

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class Sum{
     public static void main(String[] args){
-        int a = 5;
-        int b = 6;
+        int a = 27;
+        int b = 10;
 
         System.out.println(a+b);
     }
